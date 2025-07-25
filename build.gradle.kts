aa4dfce81d98386d289315acf517dfc7f7b8366e
Christopher Brown <christopher.brown2@vonage.com>
2025-07-25 10:32:24 +0100

    c

---
+++ build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.9.25"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.9.25"
+    id("com.google.devtools.ksp") version "1.9.25-1.0.20"
+    id("io.micronaut.application") version "4.5.4"
+    id("com.gradleup.shadow") version "8.3.7"
+    id("io.micronaut.aot") version "4.5.4"
+}
+
+version = "0.1"
+group = "com.example"
+
+val kotlinVersion = project.properties.get("kotlinVersion")
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    ksp("io.micronaut.serde:micronaut-serde-processor")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
+    implementation("io.micronaut.serde:micronaut-serde-jackson")
+    implementation("io.micronaut:micronaut-http-client")
+    testImplementation("io.micronaut.test:micronaut-test-rest-assured")
+    testImplementation("net.javacrumbs.json-unit:json-unit:4.1.1")
+}
+
+
+application {
+    mainClass = "com.example.ApplicationKt"
+}
+java {
+    sourceCompatibility = JavaVersion.toVersion("17")
+}
+
+
+graalvmNative.toolchainDetection = false
+
+micronaut {
+    version("4.9.0")
+    runtime("netty")
+    testRuntime("junit5")
+    processing {
+        incremental(true)
+        annotations("com.example.*")
+    }
+    aot {
+        // Please review carefully the optimizations enabled below
+        // Check https://micronaut-projects.github.io/micronaut-aot/latest/guide/ for more details
+        optimizeServiceLoading = false
+        convertYamlToJava = false
+        precomputeOperations = true
+        cacheEnvironment = true
+        optimizeClassLoading = true
+        deduceEnvironment = true
+        optimizeNetty = true
+        replaceLogbackXml = true
+    }
+}
---
+++ src/main/kotlin/com/example/SampleController.kt
@@ -0,0 +1,43 @@
+package com.example
+
+import io.micronaut.context.annotation.Primary
+import io.micronaut.core.convert.exceptions.ConversionErrorException
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Body
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Post
+import io.micronaut.http.server.exceptions.ExceptionHandler
+import io.micronaut.serde.annotation.Serdeable
+import io.micronaut.serde.exceptions.SerdeException
+import jakarta.inject.Singleton
+
+@Controller("/")
+class SampleController {
+
+    @Post("/invalid-payload") fun payloadError(@Body body: SamplePayload) = "hello"
+
+    @Serdeable
+    data class SamplePayload(
+        val stringA: String?,
+        val stringB: String?
+    )
+}
+
+@Singleton
+@Primary
+class ErrorHandler: ExceptionHandler<ConversionErrorException, HttpResponse<*>> {
+    override fun handle(request: HttpRequest<*>, exception: ConversionErrorException): HttpResponse<*> {
+        return HttpResponse.badRequest(InvalidParametersResponsePayload(
+            message = exception.message,
+            path = (exception.cause as? SerdeException)?.path?.map { it.argument.name }
+        ))
+    }
+}
+
+@Serdeable
+data class InvalidParametersResponsePayload(
+    val message: String?,
+    val path: List<String>?
+)
+
